57205fc0eefeeddef961469d0e5b96fcba2b3f1f
Monica <monica@DESKTOP-AK9SUM.localdomain>
2024-04-23 02:03:16 -0700

    done with the lab

--- server/config.py
+++ server/config.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_bcrypt import Bcrypt
+from flask_bcrypt import Bcrypt  
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
@@ -15,5 +15,4 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 bcrypt = Bcrypt(app)
-
 api = Api(app)
